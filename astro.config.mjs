2204fad831fc800c856945d67e0de1f9eeb72b2a
Sebastián Piñango <elsebastianpinango@gmail.com>
2025-12-12 11:34:11 -0400

    still fixing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static', // Change to server
+  output: 'static',
   vite: {
     resolve: {
       alias: {
-        '@': path.resolve('./src'),
-        '@components': path.resolve('./src/components'),
-        '@layouts': path.resolve('./src/layouts'),
-        '@styles': path.resolve('./src/styles'),
-        '@assets': path.resolve('./src/assets'),
-        '@lib': path.resolve('./src/lib')
+        '@': new URL('./src', import.meta.url).pathname,
+        '@components': new URL('./src/components', import.meta.url).pathname,
+        '@layouts': new URL('./src/layouts', import.meta.url).pathname,
+        '@styles': new URL('./src/styles', import.meta.url).pathname,
+        '@assets': new URL('./src/assets', import.meta.url).pathname,
+        '@lib': new URL('./src/lib', import.meta.url).pathname,
       }
     },
     plugins: [tailwindcss()]
   },
   integrations: [react()],
-  server: {
-    headers: {
-      "Content-Security-Policy": "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:"
-    }
-  },
 });
